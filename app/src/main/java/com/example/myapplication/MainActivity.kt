11bde3ac82444ea97195ad6c89bba617bca3d26c
valme24 <vansh.goel.0403@gmail.com>
2025-07-19 16:46:31 +0530

    add simpple button functioning

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextClock
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -31,14 +33,22 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 //    }
 //}
 class MainActivity : ComponentActivity() {
-    private lateinit var button : Button
+private lateinit var button :Button
+private lateinit var clock: TextClock
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity)
         button = findViewById(R.id.button)
-        button.text = getString(R.string.loginb)
+        clock = findViewById(R.id.textClock)
+        button.text = getString(R.string.button)
+
+
         }
+    fun show(view :View){
+        clock.visibility = View.VISIBLE
+
+    }
 
     }
 
